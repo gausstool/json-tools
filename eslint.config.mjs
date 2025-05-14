96408b496880af55e1a665e627f8838dd9637999
gausszhou <gausszhou@qq.com>
2025-05-14 19:44:56 +0800

    build: 部署压缩包

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,7 @@ export default defineConfig([
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
       'no-useless-escape': 'off',
     }
   }
