2aaa43f975f68fd640d60f84ad14a90716b159c4
gausszhou <gausszhou@qq.com>
2025-09-21 00:00:32 +0800

    feat: 添加 GitHub 徽章组件并集成到主页面
    添加了一个固定在右下角的 GitHub 徽章组件，点击可跳转到项目仓库。组件包含悬停效果和渐变色背景。

---
+++ src/components/github-badge.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="github-badge">
+    <a :href="url">Github</a>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  url: {
+    type: String,
+    default: "https://github.com/gausszhou/",
+  },
+});
+</script>
+
+<style scoped>
+.github-badge {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  width: 80px;
+  height: 80px;
+  display: flex;
+  background-image: linear-gradient(
+    135deg,
+    transparent 0%,
+    transparent 50%,
+    #43b984 50%,
+    #43b984 100%
+  );
+}
+.github-badge a {
+  font-size: 16px;
+  margin-left: 36px;
+  margin-top: 36px;
+  transform: rotate(-45deg);
+  font-weight: 700;
+  text-decoration: none;
+  color: black;
+}
+.github-badge:hover {
+  background-image: linear-gradient(
+    135deg,
+    transparent 0%,
+    transparent 50%,
+    #34445c 50%,
+    #34445c 100%
+  );
+}
+.github-badge:hover a {
+  color: #fff;
+}
+</style>
